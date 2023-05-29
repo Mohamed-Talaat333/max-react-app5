2aba000f528055c759be6db2b6398a786b9bf879
Mohamed Talaat <mohamedtalaat333@gmail.com>
2023-05-29 13:55:49 +0300

    fetching meals from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,56 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+  const [mealsItems, setMealsItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMealsHandler = () => {
+    setIsLoading(true);
+
+    fetch("https://react-food-order-app-cce41-default-rtdb.firebaseio.com/meals.json")
+      .then((response) => {
+        const data = response.json();
+        return data;
+      })
+      .then((data) => {
+        let loadedMeals = [];
+
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+
+        console.log(data);
+        console.log(loadedMeals);
+
+        setMealsItems(loadedMeals);
+        setIsLoading(false);
+      }).catch(err => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchMealsHandler();
+  }, []);
+
+  const mealsList = mealsItems.map((meal) => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 
   return (
     <section className={classes.meals}>
       <Card>
         <ul>{mealsList}</ul>
+        {isLoading && <p>Loading ...</p>}
+        {error && <p>{error}</p>}
       </Card>
     </section>
   );
